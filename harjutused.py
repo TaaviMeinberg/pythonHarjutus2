43b514eefd3cff982c178bd6bdc795248e463dec
jallgit72 <jalmar.siimon@gmail.com>
2021-01-26 14:20:18 +0200

    püüdsin lahendada ja Taigo õpetas. H1 ja H2

--- harjutused.py
+++ harjutused.py
@@ -7,7 +7,7 @@
 
 # Harjutus 2 - prindi negatiivsed arvud
 # Ette on antud arvude list
-arvud = [5, 9, 1, -2, 6, -15, -20]
+#arvud = [5, 9, 1, -2, 6, -15, -20]
 
 # Looge funktsioon, mis käib tsükliga kõik arvud listis läbi ning iga arvu puhul kontrollib, kas arv on negatiivne.
     # Kui arv on negatiivne, siis printige see välja
@@ -22,3 +22,24 @@ arvud = [5, 9, 1, -2, 6, -15, -20]
 #$$$
 #$$$$
 #$$$
+
+#H1.
+
+def number (arv):
+    if arv > 0 and arv < 100:
+        print ("Arv on vahemikus 0st 100ni")
+    elif arv > 101 and arv < 1000:
+        print ("Arv on vahemikus 101st 1000ni")
+number(5)
+
+#H2.
+
+arvud = [5, 9, 1, -2, 6, -15, -20]
+
+
+
+for arv in arvud:
+    if arv < 0:
+        print(arv)
+
+
